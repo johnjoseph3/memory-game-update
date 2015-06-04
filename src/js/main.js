5fac7c8eb7e18c4fe7e5ca747347195285161f1f
John Joseph <jmj1444@gmail.com>
2015-06-04 16:21:36 -0400

    made easy and hard modes

---
+++ src/js/main.js
@@ -0,0 +1,22 @@
+$(function() {
+
+	var easy = 18;
+	var hard = 24;
+
+  $('#header-container').html($('#gameboard-header').html());
+
+	function boardPopulate (squareNum) {
+		var i = 0;
+
+		while ( i < squareNum) {
+			$('.game-board').append($('#game-square').html());
+		i++;
+		};
+	};
+
+	boardPopulate(hard);
+
+  
+
+
+});
