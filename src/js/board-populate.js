8f5caf06fb597e9e9d62bf37fa605c9d3bd9173c
John Joseph <jmj1444@gmail.com>
2015-06-04 23:02:20 -0400

    about to work on hard mode

--- src/js/board-populate.js
+++ src/js/board-populate.js
@@ -1,46 +1,33 @@
 app.boardPopulate = function () {
 
-	var i = 0;
-    
+	var arr = ['a', 'a', 'b', 'b', 'c', 'c', 'd', 'd', 'e', 'e', 'f', 'f', 'g', 'g', 'h', 'h', 'i', 'i'];
 
+	function shuffleArray(array) {
+  	for (var i = array.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+  	};
+  		return array;
+	};
 
-		// while ( i < difficulty) {
-		// 	$('.game-board').append($('#game-square').html());
-		// i++;
-		// };
+  var shuffledArray = shuffleArray(arr);
+  
+  var i = 0;
 
-		var arr = ['a', 'a', 'b', 'b', 'c', 'c', 'd', 'd', 'e', 'e', 'f', 'f', 'g', 'g'];
+	while ( i < 18 ) {
 
+		var arrIcon = shuffledArray[0];
 
-		function shuffleArray(array) {
-    	for (var i = array.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    	};
-    		return array;
-		};
-
-	  var shuffledArray = shuffleArray(arr);
-	  
-	  
-		while ( i < 14	) {
-
-			var arrIcon = shuffledArray[0];
-
-			var squareHtml = '<label class="tile"><input type="checkbox" class="checkbox"><span class="card animated"><span class="icon">'+ arrIcon +'</span><span></label>'	
-			
-			$('.game-board').append(squareHtml);		
-
-			shuffledArray.shift();
-
-		i++; 
-
-		};
-
-
+		var squareHtml = '<label class="tile"><input type="checkbox" class="checkbox"><span class="card animated"><span class="icon">'+ arrIcon +'</span><span></label>'	
 		
+		$('.game-board').append(squareHtml);		
 
+		shuffledArray.shift();
 
-}
+		i++;	 
+
+	};
+
+};
