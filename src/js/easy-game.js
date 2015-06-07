e6b9bd0a230c5c85d7d2dfa145fa643b904dd497
John Joseph <jmj1444@gmail.com>
2015-06-07 17:38:41 -0400

    fixed number of squares for easy page

--- src/js/easy-game.js
+++ src/js/easy-game.js
@@ -1,12 +1,10 @@
 app.router.route('easy', function () {
 
-	var easy = 6;
+	var easy = 18;
 
 	app.generateHeader('easy');
 
-	var easyArr = ['a', 'a', 'b', 'b', 'c', 'c']
-
-	// ,'d', 'd', 'e', 'e', 'f', 'f' , 'g', 'g', 'h', 'h', 'i', 'i'];
+	var easyArr = ['a', 'a', 'b', 'b', 'c', 'c','d', 'd', 'e', 'e', 'f', 'f' , 'g', 'g', 'h', 'h', 'i', 'i']
 
 	app.boardPopulate(easyArr, easy);
 
