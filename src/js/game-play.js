7722ee5eec9e77d0e2fd3c6d2d68e2f4774cabaa
John Joseph <jmj1444@gmail.com>
2015-06-07 15:58:52 -0400

    added a function for removing hearts

--- src/js/game-play.js
+++ src/js/game-play.js
@@ -13,23 +13,27 @@ app.gamePlay = function () {
 	  var tileContent = this.textContent;
 	  guessArr.push(tileContent);
 
-	  if (guessArr.length === 2 && (guessArr[0] === guessArr[1])) {
-	    console.log('they match');
+	  var hearts = $('.hearts').text();
+	  
+	  var removeHeart = function () {
+	  	var sliceHearts = hearts.slice(0, -1);
+	  	 
+	  	 $('.hearts').text(sliceHearts);
+	  };
 
-      guessArr = [];
-  	  guessTile = [];
+	  if (guessArr.length === 2 && (guessArr[0] === guessArr[1])) {
+	      console.log('they match');
+
+        guessArr = [];
+  	    guessTile = [];
 
 	  } else if (guessArr.length === 2 && (guessArr[0] != guessArr[1])) {
 	  	  console.log('they dont match'); 
-	  	  var hearts = $('.hearts').text();
-	  	  var sliceHearts = hearts.slice(0, -1);
-	  	  console.log(sliceHearts);
-	  	  $('.hearts').text(sliceHearts);
-	  	 
-	  	app.unCheck(firstTile, secondTile);
-
-	  	guessArr = [];
-	    guessTile = [];
+	  	  removeHeart(); 
+	  	  app.unCheck(firstTile, secondTile);
+  
+	  	  guessArr = [];
+	      guessTile = [];
 	   }  	    		 
 	})
 
