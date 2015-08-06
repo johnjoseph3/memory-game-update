6507ec58168553b316e7cac7d10e17ce2927342c
John Joseph <jmj1444@gmail.com>
2015-08-06 18:15:26 -0400

    fixed routing problem

--- src/js/start-screen.js
+++ src/js/start-screen.js
@@ -2,6 +2,13 @@ app.router.route('', function () {
 
 	$('body').html($('#start-page').html());
 
+	$('.home-btns').on('click', function() {
+		setTimeout(function() {
+			window.location.reload();
+		}, 500);
+				
+	});
+
 });
 
   
--- src/js/un-bind.js
+++ src/js/un-bind.js
@@ -2,14 +2,8 @@ app.unBind = function (tile1, tile2) {
 	
 	setTimeout(function(){ 
 		
-
-
 		$('.card').off('click');
 
-		
-
-console.log(tile1)
-console.log(tile2)
 
 	 }, 1000);
 	
